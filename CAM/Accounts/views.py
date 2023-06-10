4422784089ee6ae69db9106f7686d9d3477079d1
nigel <nigelchimwene@gmail.com>
2023-06-10 13:48:50 +0300

    templates

--- CAM/Accounts/views.py
+++ CAM/Accounts/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
+
+
 def home(request):
     orders = Order.objects.all()
-    customer = customer.objects.all()
+    # customer = customer.objects.all()
 
-    context = {'orders':orders,'customer':customer}
-
+    # context = {'orders':orders,'customer':customer}
 
     return render(request, 'Accounts/dashboard.html')
 
@@ -17,6 +18,7 @@ def products(request):
     return render(request, 'Accounts/products.html',{'products':products})
 
 def customer(request):
-    # customer = customer.objects.all()
-    return render(request,'Accounts/customer.html')
+    customer = customer.objects.all()
+    return render(request,'Accounts/customer.html',{'customer':customer})
+
 
