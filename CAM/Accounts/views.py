9079555df4f32ea194beb95e3e5736c416a14533
nigel <nigelchimwene@gmail.com>
2023-06-12 13:51:57 +0300

    views

--- CAM/Accounts/views.py
+++ CAM/Accounts/views.py
@@ -8,7 +8,14 @@ from  .models import *
 def home(request):
     orders = Order.objects.all()
     customer = Customer.objects.all()
-    context = {'orders':orders,'customer':customer}
+
+    total_customers = customer.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'deliverd').count()
+    pending = orders.filter(status = 'pending').count()
+
+    context = {'orders':orders,'customer':customer,'total_customers':total_customers,
+                'total_orders':total_orders,'delivered':delivered,'pending':pending}
 
     return render(request, 'Accounts/dashboard.html',context)
 
