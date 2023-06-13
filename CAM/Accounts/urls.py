d66b2d2fb136ac688c1d84a31dc3e86947432c13
nigel <nigelchimwene@gmail.com>
2023-06-13 12:37:56 +0300

    urls

--- CAM/Accounts/urls.py
+++ CAM/Accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('products/',views.products,name='products'),
-    path('customer/<str:pk_test>/',views.customer),
+    path('customer/<str:pk_test>/',views.customer,name='customer'),
 ]
