1a1cbd6a25539321f81e7fecd38b9130c7eab9a2
nigel <nigelchimwene@gmail.com>
2023-06-15 13:57:03 +0300

    forms

--- CAM/Accounts/views.py
+++ CAM/Accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from  .models import *
 from .forms import OrderForm
 
@@ -33,17 +34,21 @@ def customer(request,pk_test):
     context = {'customer':customer,'orders':orders,'order_count':order_count}
     return render(request,'Accounts/customer.html',context)
 
-def createOrder(request):
+def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'),extra=10)
 
-    form = OrderForm()
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset= Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
+        # form = OrderForm(request.POST)
         # print('Printing POST',request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
     
-    context = {'form':form}
+    context = {'formset':formset}
         
     return render(request,'Accounts/order_form.html',context)
 
