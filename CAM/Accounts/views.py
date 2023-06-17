441eeb0066d313feecc22ce4a3f976e03b4948bd
nigel <nigelchimwene@gmail.com>
2023-06-17 15:28:41 +0300

    filters

--- CAM/Accounts/views.py
+++ CAM/Accounts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from  .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -31,7 +32,11 @@ def customer(request,pk_test):
     orders = customer.order_set.all()
     order_count = orders.count()
 
-    context = {'customer':customer,'orders':orders,'order_count':order_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+
+    context = {'customer':customer,'orders':orders,'order_count':order_count,'myFilter':myFilter}
     return render(request,'Accounts/customer.html',context)
 
 def createOrder(request,pk):
--- CAM/CAM/settings.py
+++ CAM/CAM/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accounts',
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
